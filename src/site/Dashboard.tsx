eae2179cf1a15a7ce72596786eccb4f49dfd439b
anchal.koshta007@gmail.com <genious@genious.genious>
2026-07-04 13:17:08 +0530

    Build a view for the Chat Window with Corrections

--- src/site/Dashboard.tsx
+++ src/site/Dashboard.tsx
@@ -5,16 +5,27 @@ import type React from "react";
 const Dashboard: React.FC = () => {
     return (
         <div className="dashboard">
-            <h1>Welcome to Dashboard</h1>
+            {/* <h1>Welcome to Dashboard</h1> */}
+
             <div className="main-window">
-                <div className="left">contacts</div>
+
+                <div className="left">
+                    <div className="header">
+                        Contacts
+                    </div>
+                    <div className="contact-body">
+                        
+                    </div>
+                </div>
+
+
                 <div className="middle">
-                    <h2>Anchal Koshta</h2>
+                    <div className="header">Anchal Koshta</div>
                     <div className="chat-pannel">
 
                     </div>
-                    <form  className="form-field">
-                        <input type="text" name="chat" id="chat" required  />
+                    <form className="form-field">
+                        <input type="text" name="chat" id="chat" required />
                         <button type="submit">Send</button>
                     </form>
                 </div>
